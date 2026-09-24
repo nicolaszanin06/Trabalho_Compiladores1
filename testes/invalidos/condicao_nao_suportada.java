0515dc289a402539a47b991814c9e825eb795cac
Nicolas Zanin <nicolaszanin2006@gmail.com>
2026-09-23 21:14:49 -0300

    test: adiciona entradas Java para o analisador

---
+++ testes/invalidos/condicao_nao_suportada.java
@@ -0,0 +1,3 @@
+if (contador) {
+    int valor;
+}
---
+++ testes/validos/comentarios.java
@@ -0,0 +1,8 @@
+// Comentario de linha
+int primeiro; /* comentario de bloco */
+
+if (true) {
+    /* Comentario
+       em varias linhas */
+    float segundo;
+}
